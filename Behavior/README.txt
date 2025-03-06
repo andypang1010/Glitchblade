973b4595229f7df8890df44b88ab2d32efe2067b
Andy Pang <andypang1010@gmail.com>
2025-03-06 16:39:25 -0500

    Create Empty Sub-Actions

--- Behavior/README.txt
+++
@@ -1,8 +0,0 @@
-Final Submission - CUGL Behavior Tree Project
-David Kim & James Liu
-
-We were unable to build our btree files with cugl so we are leaving them
-in the source folder to expose them to the demo files.
-
-There are two demo apps in main: dogApp and fighterApp. Comment one out to switch
-between them.
---
+++ source/GBMovementActionModel.h
@@ -0,0 +1,73 @@
+#ifndef ACTION_MODEL_H
+#define ACTION_MODEL_H
+
+#include <string>
+#include <cugl/cugl.h>
+
+using namespace cugl;
+
+/**
+ * An abstract class representing an action model with an animation and duration.
+ */
+class ActionModel {
+protected:
+    /** The name of the action */
+    std::string _actionName;
+
+    /** The animation for the action. */
+    std::shared_ptr<cugl::graphics::SpriteSheet> _actionAnimation;
+
+    /** The length/duration of the action. */
+    float _actionLength;
+
+public:
+    /** Default constructor. */
+    ActionModel();
+
+    /** Virtual destructor. */
+    virtual ~ActionModel();;
+
+    /**
+     * Returns the action animation.
+     *
+     * @return The shared pointer to the SpriteSheet.
+     */
+    std::shared_ptr<cugl::graphics::SpriteSheet> getActionAnimation() const;
+
+    /**
+     * Sets the action animation.
+     *
+     * @param animation The shared pointer to the new SpriteSheet.
+     */
+    void setActionAnimation(const std::shared_ptr<cugl::graphics::SpriteSheet>& animation);
+
+    /**
+     * Returns the action length.
+     *
+     * @return The length of the action.
+     */
+    float getActionLength() const;
+
+    /**
+     * Sets the action length.
+     *
+     * @param length The new action length.
+     */
+    void setActionLength(float length);
+
+    /**
+     * Returns the action name.
+     *
+     * @return The name of the action.
+     */
+    std::string getActionName() const;
+
+    /**
+     * Sets the action name.
+     *
+     * @param name The new action name.
+     */
+    void setActionName(const std::string& name);
+};
+
+#endif // ACTION_MODEL_H
