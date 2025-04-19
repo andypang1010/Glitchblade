85b78e4aae04e0d68bd837334caea13045e762da
dw642 <dw642@cornell.edu>
2025-04-19 15:36:13 -0400

    changed minion1b slam attack back

--- source/enemies/enemy_variants/GBMinion1BModel.cpp
+++ source/enemies/enemy_variants/GBMinion1BModel.cpp
@@ -269,7 +269,7 @@ void Minion1BModel::updateAnimation()
     playAnimationOnce(_punchSprite);
     playAnimationOnce(_stunSprite);
 
-	playVFXAnimation(_slamSprite, _slamVFXSprite, 29);
+	playVFXAnimation(_slamSprite, _slamVFXSprite, 19);
 
     _node->setScale(Vec2(isFacingRight() ? 1 : -1, 1));
     _node->getChild(_node->getChildCount() - 2)->setScale(Vec2(isFacingRight() ? 1 : -1, 1));
